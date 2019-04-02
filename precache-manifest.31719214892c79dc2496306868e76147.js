9e9532416b8c22803374eb4f1bb77daafa942f48
Jaikanth <jaikanthjay46@gmail.com>
2019-04-03 01:49:23 +0530

    Updates

---
+++ precache-manifest.31719214892c79dc2496306868e76147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PhoneBook/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eadacd478a93671e1e3d",
+    "url": "/PhoneBook/static/js/runtime~main.57ff495b.js"
+  },
+  {
+    "revision": "3c6bd563e5d087ec93d9",
+    "url": "/PhoneBook/static/js/main.cf46809a.chunk.js"
+  },
+  {
+    "revision": "efdc10d946ad6b2aaf0b",
+    "url": "/PhoneBook/static/js/2.a2ab41fa.chunk.js"
+  },
+  {
+    "revision": "3c6bd563e5d087ec93d9",
+    "url": "/PhoneBook/static/css/main.b4b03f78.chunk.css"
+  },
+  {
+    "revision": "efdc10d946ad6b2aaf0b",
+    "url": "/PhoneBook/static/css/2.0bc1ba23.chunk.css"
+  },
+  {
+    "revision": "e226493ba5dbd8d6e05eb494d03d0917",
+    "url": "/PhoneBook/index.html"
+  }
+];
